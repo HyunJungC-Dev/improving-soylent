93110d89a72425104abecea60c288604bd3546d2
HyunJung <HyunJungC.Dev@gmail.com>
2021-09-02 14:46:35 +0900

    config: Set eslint and prettier in typescript and react
    - eslint 와 prettier를 typescript와 react에 맞게 설정

--- src/components/A11yHidden/A11yHidden.tsx
+++ src/components/A11yHidden/A11yHidden.tsx
@@ -2,17 +2,17 @@ import { ComponentType } from 'react';
 import { classNames } from 'utils';
 
 type A11yHiddenProps = {
-  as: string | ComponentType<{className:string}>,
-  className?: string,
-  children:string,
-  href?:string
-}
+  as: string | ComponentType<{ className: string }>;
+  className?: string;
+  children: string;
+  href?: string;
+};
 
-A11yHidden.defaultProps ={
+A11yHidden.defaultProps = {
   as: 'span',
   className: ''
-}
+};
 
-export function A11yHidden({ as:Comp, className, ...restProps }: A11yHiddenProps) {
-  return <Comp className={classNames('a11yHidden')(className)} {...restProps} />
+export function A11yHidden({ as: Comp, className, ...restProps }: A11yHiddenProps) {
+  return <Comp className={classNames('a11yHidden')(className)} {...restProps} />;
 }
