d963ac0752e81ed3ca98010c654c8a9a16520421
HyunJung <HyunJungC.Dev@gmail.com>
2021-09-06 14:46:07 +0900

    feat: Layout product card
    - 프로덕트 카드 안 레이아웃을 잡음

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { A11yHidden } from 'components';
 import { classNames } from 'utils';
-import { useState } from 'react';
+// import { classNames } from 'utils';
+// import { useState } from 'react';
 import styles from './ProductCard.module.css';
 
 type ProductCardProps = {
@@ -27,44 +28,56 @@ export function ProductCard({
 }: ProductCardProps) {
   const oneTimePurchasePrice = (+discountVariantPrice / (1 - +discountPercentage / 100)).toFixed(2);
 
+  const imgWidth = 250;
   return (
     <>
-      <a href={url}>
-        <figure className="resetFigure">
-          <img src={featuredImageSrc.replace('{width}', '250')} alt={title} title={title} />
-          <figcaption>
-            {title}
+      <li className={styles.productCard}>
+        <a href={url}>
+          <figure className="resetFigure">
+            <img
+              className={styles.productImg}
+              src={featuredImageSrc.replace('{width}', imgWidth.toString())}
+              alt={title}
+              title={title}
+            />
+            <figcaption className={styles.productTitle}>{title}</figcaption>
+          </figure>
+          <p className={styles.productSimpleInfo}>
             <strong>
               {bulkQuantity + ' ' + unitName.charAt(0).toUpperCase() + unitName.slice(1) + 's'}
             </strong>
             <em>별점</em>
-          </figcaption>
-        </figure>
-      </a>
+          </p>
+        </a>
 
-      <A11yHidden as="span" id="purchaseOption-label">
-        Purchase Option
-      </A11yHidden>
-      <ul
-        id="rg-purchaseOption"
-        className={styles.radiogroup}
-        role="radiogroup"
-        aria-labelledby="label-purchaseOption"
-        aria-activedescendant="rb-subscribeAndSave"
-        tabIndex={0} // typescript에서는 tabindex를 {}로 넣어줘야 한다.
-      >
-        <li id="rb-subscribeAndSave" role="radio" aria-checked="false">
-          Subscribe & Save
-          <strong> {'$' + discountVariantPrice}</strong>
-          <em>{'(' + (+discountVariantPrice / +bulkQuantity).toFixed(2) + '/' + unitName + ')'}</em>
-        </li>
-        <li id="rb-oneTimePurchase" role="radio" aria-checked="false">
-          {' '}
-          One-time Purchase
-          <strong> {'$' + oneTimePurchasePrice}</strong>
-          <em>{'(' + (+oneTimePurchasePrice / +bulkQuantity).toFixed(2) + '/' + unitName + ')'}</em>
-        </li>
-      </ul>
+        <A11yHidden as="span" id="purchaseOption-label">
+          Purchase Option
+        </A11yHidden>
+        <ul
+          id="rg-purchaseOption"
+          className={styles.radiogroup}
+          role="radiogroup"
+          aria-labelledby="label-purchaseOption"
+          aria-activedescendant="rb-subscribeAndSave"
+          tabIndex={0} // typescript에서는 tabindex를 {}로 넣어줘야 한다.
+        >
+          <li id="rb-subscribeAndSave" role="radio" aria-checked="false">
+            Subscribe & Save
+            <strong> {'$' + discountVariantPrice}</strong>
+            <em>
+              {'(' + (+discountVariantPrice / +bulkQuantity).toFixed(2) + '/' + unitName + ')'}
+            </em>
+          </li>
+          <li id="rb-oneTimePurchase" role="radio" aria-checked="false">
+            {' '}
+            One-time Purchase
+            <strong> {'$' + oneTimePurchasePrice}</strong>
+            <em>
+              {'(' + (+oneTimePurchasePrice / +bulkQuantity).toFixed(2) + '/' + unitName + ')'}
+            </em>
+          </li>
+        </ul>
+      </li>
     </>
   );
 }
