2b9718c1bf506940da3ee230ea92b4e85d93b429
HyunJung <HyunJungC.Dev@gmail.com>
2021-09-07 21:16:37 +0900

    feat: Add carousel left arrow, right arrow function
    - 왼쪽 화살표를 누르면 3칸 앞으로 이동
    - 오른쪽 화살푤르 누르면 3칸 뒤로 이동
    - 첫 프로덕트인 경우 왼쪽 화살표를 눌러도 미반응
    - 마지막 프로덕트인 경우 오른쪽 화살표를 눌러도 미반응

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -40,6 +40,10 @@ export type CarouselProps = {
 };
 
 export function Carousel({ carouselList }: CarouselProps) {
+  const PRODUCTLIST_NUM = 3;
+  const PRODUCTCARD_WIDTH = 300; // px
+  const PRODUCT_MARGIN_RIGHT = 20; // px
+
   const productCardContainer = useRef<HTMLUListElement>(null);
   const [sliderIndex, setSliderIndex] = useState(0);
 
@@ -47,7 +51,9 @@ export function Carousel({ carouselList }: CarouselProps) {
     const { current } = productCardContainer;
     if (current) {
       current.style.transition = '1s';
-      current.style.transform = `translate3d(${sliderIndex * -320 * 3}px,0,0)`;
+      current.style.transform = `translate3d(${
+        sliderIndex * -(PRODUCTCARD_WIDTH + PRODUCT_MARGIN_RIGHT) * PRODUCTLIST_NUM
+      }px,0,0)`;
     }
   });
 
@@ -70,12 +76,22 @@ export function Carousel({ carouselList }: CarouselProps) {
         role="button"
         className={classNames('svgButton')('leftbutton')}
         onClick={() => {
-          setSliderIndex(sliderIndex => sliderIndex + 1);
+          setSliderIndex(sliderIndex => (sliderIndex > 0 ? sliderIndex - 1 : sliderIndex));
         }}
       >
         <SvgIconLeftArrow width="100" height="100" />
       </span>
-      <span role="button" className={classNames('svgButton')('rightbutton')}>
+      <span
+        role="button"
+        className={classNames('svgButton')('rightbutton')}
+        onClick={() => {
+          setSliderIndex(sliderIndex =>
+            sliderIndex < Math.floor(carouselList.length / PRODUCTLIST_NUM) - 1
+              ? sliderIndex + 1
+              : sliderIndex
+          );
+        }}
+      >
         <SvgIconRightArrow width="100" height="100" />
       </span>
     </>
