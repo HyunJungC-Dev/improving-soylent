e5cb85b51b77ad2fa3364fb463ce298c826fe91a
HyunJung <HyunJungC.Dev@gmail.com>
2021-09-08 17:55:11 +0900

    feat: Seperate static data
    - 정적 데이터가 길어서 따로 파일로 분리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { Navigation, Tab } from 'components';
 import { useState, useEffect } from 'react';
+import { navigationList, productTabList } from './staticData';
 
 type productListsType = {
   'all-drinks': [];
@@ -8,60 +9,6 @@ type productListsType = {
 };
 
 function App() {
-  const navigationList = [
-    {
-      id: 'drinks',
-      dropdown: true,
-      dropdownList: [
-        {
-          category: 'Complete Meal',
-          description: 'No time, no problem! Complete, drinkable meal.'
-        },
-        {
-          category: 'Complete Protein',
-          description: 'Smooth and creamy high protein + nutrition.'
-        },
-        {
-          category: 'Complete Energy',
-          description: 'Brain and body boost without the crash.'
-        }
-      ],
-      dropdownLinks: ['Complete Meal', 'Complete Protein', 'Complete Energy']
-    },
-    {
-      id: 'powders',
-      dropdown: true,
-      dropdownList: [
-        {
-          category: 'Soylent Powder',
-          description: 'Scoop, shake, go! Complete meal.\nGoodbye hunger'
-        },
-        {
-          category: 'Blender Bottle',
-          description: 'Every great meal needs a great vessel'
-        },
-        {
-          category: 'Powder Scoop',
-          description: 'Who wants to measure?Scoop and go!'
-        }
-      ],
-      dropdownLinks: ['Shop All', 'Gift Cards']
-    },
-    { id: 'bars', dropdown: false },
-    { id: 'shop_all', dropdown: false }
-  ].map(nav => ({
-    ...nav,
-    href: '/' + nav.id.replace('_', ''),
-    text: nav.id.replace('_', ' ').toUpperCase()
-  }));
-
-  const productTabList = ['Drinks', 'Powder', 'Squared'].map(tabName => ({
-    id: tabName.toLowerCase(),
-    displayText: tabName,
-    href: `/${tabName.toLowerCase()}`,
-    documentTitle: `Home | ${tabName}`
-  }));
-
   const [productLists, setProductLists] = useState<productListsType>({
     'all-drinks': [],
     'all-powder': [],
---
+++ src/app/staticData.tsx
@@ -0,0 +1,53 @@
+export const navigationList = [
+  {
+    id: 'drinks',
+    dropdown: true,
+    dropdownList: [
+      {
+        category: 'Complete Meal',
+        description: 'No time, no problem! Complete, drinkable meal.'
+      },
+      {
+        category: 'Complete Protein',
+        description: 'Smooth and creamy high protein + nutrition.'
+      },
+      {
+        category: 'Complete Energy',
+        description: 'Brain and body boost without the crash.'
+      }
+    ],
+    dropdownLinks: ['Complete Meal', 'Complete Protein', 'Complete Energy']
+  },
+  {
+    id: 'powders',
+    dropdown: true,
+    dropdownList: [
+      {
+        category: 'Soylent Powder',
+        description: 'Scoop, shake, go! Complete meal.\nGoodbye hunger'
+      },
+      {
+        category: 'Blender Bottle',
+        description: 'Every great meal needs a great vessel'
+      },
+      {
+        category: 'Powder Scoop',
+        description: 'Who wants to measure?Scoop and go!'
+      }
+    ],
+    dropdownLinks: ['Shop All', 'Gift Cards']
+  },
+  { id: 'bars', dropdown: false },
+  { id: 'shop_all', dropdown: false }
+].map(nav => ({
+  ...nav,
+  href: '/' + nav.id.replace('_', ''),
+  text: nav.id.replace('_', ' ').toUpperCase()
+}));
+
+export const productTabList = ['Drinks', 'Powder', 'Squared'].map(tabName => ({
+  id: tabName.toLowerCase(),
+  displayText: tabName,
+  href: `/${tabName.toLowerCase()}`,
+  documentTitle: `Home | ${tabName}`
+}));
